65f29445fc8da49e881dde9d904661e0edaf9b6a
raimonmerce <raimon.merc.gots@gmail.com>
2023-10-19 11:49:46 +0900

    V2

--- js/script.js
+++ js/script.js
@@ -3,11 +3,13 @@ import Translate from './translate.js';
 let sections = ["home", "about", "newsletter", "gallery", "contact"]
 let translator;
 let gallery = [];
+let selectedGallery = [];
+let unselectedGallery = [];
+let gridId = {};
 let modal;
-let slideIndex;
+let slideIndex = 1;
 
 function GoTo(section) {
-    console.log(section)
     let i, tabcontent, tablinks;
     tabcontent = document.getElementsByClassName("sectioncontent");
     for (i = 0; i < tabcontent.length; i++) {
@@ -19,6 +21,15 @@ function GoTo(section) {
     }
     document.getElementById(section).style.display = "block";
     document.getElementById(section+"_nav").className += " nav_active";
+
+    if (section == "gallery"){
+        var acc = document.getElementsByClassName("accordion");
+
+        for (i = 0; i < acc.length; i++) {
+            var panel = acc[i].nextElementSibling;
+            panel.style.maxHeight = panel.scrollHeight + "px";
+        }
+    }
 }
 
 function copyEmail(){
@@ -58,19 +69,25 @@ function showSlides(n) {
     dots[slideIndex-1].className += " active";
 }
 
+function toggleAccordion(elem){
+    elem.classList.toggle("accordion_active");
+    var panel = elem.nextElementSibling;
+    let val = parseInt(panel.style.maxHeight.replace("px", ""));
+
+    if (!panel.style.maxHeight || val > 0) {
+        panel.style.maxHeight = 0;
+    } else {
+        panel.style.maxHeight = panel.scrollHeight + "px";
+    }
+}
+
 function initializeGallery(){
     var acc = document.getElementsByClassName("accordion");
     var i;
 
     for (i = 0; i < acc.length; i++) {
         acc[i].addEventListener("click", function() {
-            this.classList.toggle("active");
-            var panel = this.nextElementSibling;
-            if (panel.style.maxHeight) {
-            panel.style.maxHeight = null;
-            } else {
-            panel.style.maxHeight = panel.scrollHeight + "px";
-            } 
+            toggleAccordion(this)
         });
     }
 
@@ -86,37 +103,10 @@ function initializeGallery(){
                 gallery = JSON.parse(xrhFile.responseText);
                 for (const key in gallery) {
                     if (gallery.hasOwnProperty(key)) {
-
-                        /*
-                        <div class="container">
-                            <img src="img_avatar.png" class="image">
-                            <div class="overlay">My Name is John</div>
-                        </div>
-                        */
                         let val = gallery[key];
                         let cuadricula = document.getElementById(val["type"]);
                         if (cuadricula){
-                            let elementoDiv = document.createElement("div");
-                            elementoDiv.className = "container";
-    
-                            let image = document.createElement("img");
-                            image.src = val["thumbnail"];
-                            image.className = "image";
-
-                            let imageName = document.createElement("div");
-                            imageName.textContent = val["name"];
-                            imageName.className = "overlay";
-    
-                            elementoDiv.appendChild(imageName);
-                            elementoDiv.appendChild(image);
-
-                            elementoDiv.addEventListener('click', () => {
-                                console.log(val)
-                                modal.style.display = "block";
-                                document.getElementById("modal-title").textContent = val["name"];
-                                document.getElementById("modal-text").textContent = val["description"];
-                                document.getElementById("modal-image").src = val["thumbnail"];
-                            });
+                            let elementoDiv = createGridElement(val)
                             cuadricula.appendChild(elementoDiv);
                         }
                     }
@@ -127,11 +117,114 @@ function initializeGallery(){
     xrhFile.send();
 }
 
+function getRandomGallery(){
+    let randomIndex = Math.floor(Math.random() * unselectedGallery.length);
+    let randomKey = unselectedGallery[randomIndex];
+    unselectedGallery.splice(randomIndex, 1);
+    selectedGallery.push = randomKey;
+    return randomKey;
+}
+
+function createGridElement(element){
+    let elementoDiv = document.createElement("div");
+    
+    elementoDiv.className = "container";
+
+    let image = document.createElement("img");
+    image.src = element["thumbnail"];
+    image.className = "image";
+    //image.classList.add('fade');
+
+    let imageName = document.createElement("div");
+    imageName.textContent = element["name"];
+    imageName.className = "overlay";
+
+    elementoDiv.appendChild(imageName);
+    elementoDiv.appendChild(image);
+
+    elementoDiv.addEventListener('click', () => {
+        modal.style.display = "block";
+        document.getElementById("modal-title").textContent = element["name"];
+        document.getElementById("modal-text").textContent = element["description"];
+        let slideshow_div = document.getElementById("slideshow-div");
+        let dots_div = document.getElementById("dots-div");
+        slideshow_div.innerHTML = "";
+        dots_div.innerHTML = "";
+        for (let i = 0; i < element["imatges"].length; ++i){
+            let image = element["imatges"][i];
+            slideshow_div.innerHTML += '<div class="mySlides fade"> <div class="numbertext">' + (i+1) + " / " + element["imatges"].length + '</div> <img src="' + image + '" style="width:100%"> </div>';
+            
+            let dot = document.createElement("span");
+            dot.classList.add('dot');
+            dot.onclick = function () {currentSlide(i+1);};
+            dots_div.appendChild(dot);
+        }
+
+        showSlides(slideIndex);
+        document.getElementById("modal-image").src = element["thumbnail"];
+    });
+
+    return elementoDiv;
+}
+
+function getRandomTime(){
+    return Math.floor(Math.random() * (10000 - 3000)) + 2000;
+}
+
+function updateElement(idElem){
+    let key = getRandomGallery();
+    let val = gallery[key];
+    let oldKey = gridId[idElem];
+    gridId[idElem] = key;
+
+    selectedGallery = selectedGallery.filter(item => item !== oldKey);
+    unselectedGallery.push(oldKey);
+
+    let oldElement = document.getElementById(idElem);
+    let element = createGridElement(val);
+    element.id = idElem;
+    //oldElement.replaceWith(element)
+    oldElement.classList.add('fadeout');
+    oldElement.style.opacity = 0;
+    setTimeout(function () {
+        oldElement.replaceWith(element);
+        oldElement.classList.remove('fadeout');
+        oldElement.classList.add('fadein');
+        oldElement.style.opacity = 1;
+    }, 2000); 
+
+    setTimeout(() => {
+        updateElement(idElem)
+    }, getRandomTime());
+    
+}
+
+function initializeHome(){
+    unselectedGallery = Object.keys(gallery);
+    let randomHome = document.getElementById("random_home");
+    for (let i = 0; i < 9; ++i){
+        let tmpId = "rand_" + i;
+        let key = getRandomGallery();
+        gridId[tmpId] = key;
+        let element = gallery[key];
+        
+        let elementoDiv = createGridElement(element);
+        elementoDiv.id = tmpId;
+        elementoDiv.classList.add('fadeout');
+  
+        setTimeout(() => {
+            updateElement(tmpId)
+        }, getRandomTime());
+
+        randomHome.appendChild(elementoDiv);
+    }
+}
+
 function translate(lng, tagAttr){
     translator = new Translate();
     translator.init(tagAttr, lng);
     translator.process();
-} //dibuixos i series, grans formats, accións i instalacions
+}
 
 function init() {
     translate('esp','lng-tag')
@@ -141,6 +234,9 @@ function init() {
     document.getElementById('gallery_nav').onclick = function () {GoTo("gallery");};
     document.getElementById('contact_nav').onclick = function () {GoTo("contact");};
     document.getElementById('copy_email').onclick = function () {copyEmail();};
+    document.getElementById('prev_slideshow').onclick = function () {plusSlides(-1);};
+    document.getElementById('next_slideshow').onclick = function () {plusSlides(1);};
+
     modal = document.getElementById("myModal");
     document.getElementsByClassName("close")[0].onclick = function() {
         modal.style.display = "none";
@@ -158,8 +254,9 @@ function init() {
     dropdown.addEventListener('change', function() {
         translate(dropdown.value,'lng-tag')
     });
-
+    
     initializeGallery()
+    initializeHome()
 
 }
 
