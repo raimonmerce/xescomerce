731facbb49f3ad7cf6d40c7e91cdf7ab38e3eb7f
raimonmerce <raimon.merc.gots@gmail.com>
2025-04-10 11:11:51 +0900

    Update

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react'
 import path from 'path';
 
 export default defineConfig({
-  base: "/xescomerce/", 
+  base: "/", 
   plugins: [
     react(),
     tailwindcss(),
