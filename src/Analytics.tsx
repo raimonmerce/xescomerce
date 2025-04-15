b81d3a0a78cd439ca99752abdd58800af2b92196
raimonmerce <raimon.merc.gots@gmail.com>
2025-04-16 07:46:02 +0900

    Env variables

---
+++ src/Analytics.tsx
@@ -0,0 +1,40 @@
+import { useEffect } from 'react';
+
+const GTM_ID = import.meta.env.VITE_GTM_ID;
+const GA_ID = import.meta.env.VITE_GA_ID;
+
+export const Analytics = () => {
+  useEffect(() => {
+    // Inject GTM
+    if (GTM_ID) {
+      const script = document.createElement('script');
+      script.innerHTML = `
+        (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+        new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+        j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+        'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+        })(window,document,'script','dataLayer','${GTM_ID}');
+      `;
+      document.head.appendChild(script);
+    }
+
+    // Inject GA
+    if (GA_ID) {
+      const gaScript = document.createElement('script');
+      gaScript.setAttribute('async', 'true');
+      gaScript.src = `https://www.googletagmanager.com/gtag/js?id=${GA_ID}`;
+      document.head.appendChild(gaScript);
+
+      const inlineScript = document.createElement('script');
+      inlineScript.innerHTML = `
+        window.dataLayer = window.dataLayer || [];
+        function gtag(){dataLayer.push(arguments);}
+        gtag('js', new Date());
+        gtag('config', '${GA_ID}');
+      `;
+      document.head.appendChild(inlineScript);
+    }
+  }, []);
+
+  return null;
+};
--- src/main.tsx
+++ src/main.tsx
@@ -3,11 +3,13 @@ import { createRoot } from "react-dom/client";
 import { HelmetProvider } from "react-helmet-async"; // Import HelmetProvider
 import "./index.css";
 import App from "./App.tsx";
+import { Analytics } from './Analytics.tsx';
 import "./i18n/i18n";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <HelmetProvider>
+      <Analytics />
       <App />
     </HelmetProvider>
   </StrictMode>
