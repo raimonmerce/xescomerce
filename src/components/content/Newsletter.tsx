9d09142625fca89fd1f7818fdfb0f30ea569d66f
raimonmerce <raimon.merc.gots@gmail.com>
2025-04-16 08:08:55 +0900

    Add news text

--- src/components/content/Newsletter.tsx
+++ src/components/content/Newsletter.tsx
@@ -1,7 +1,6 @@
 import React, {useState} from "react";
 import { useTranslation } from "react-i18next";
 import { NewsManager } from '../../data/NewsManager';
-//import NewsThumbnail from "../artwork/NewsThumbnail";
 import FrameThumbnail from "../artwork/FrameThumbnail";
 
 interface NewsletterProps {
@@ -24,6 +23,7 @@ const Newsletter: React.FC<NewsletterProps> = ({ setOpenPopup }) => {
         <p className="text-[25px] md:text-[30px] px-4 text-center whitespace-nowrap">{t("header.newsletter")}</p>
         <div className="flex-grow border-t border-gray-300"></div>
       </div>
+      <p className="flex-grow text-[20px] md:text-[25px] text-center mb-[20px]">{t("newsletter.description")}</p>
       <div className="grid grid-cols-1 lg:grid-cols-3 gap-4">
         {news.map((item, num) => {
           const newItem = newsManager.getById(item);
@@ -38,7 +38,6 @@ const Newsletter: React.FC<NewsletterProps> = ({ setOpenPopup }) => {
             isBig={true}
             date={formattedDate}
           />
-          // <NewsThumbnail key={num} id={item} setOpenPopup={setOpenPopup} />
         )})}
       </div>
     </>
--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,6 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import { HelmetProvider } from "react-helmet-async"; // Import HelmetProvider
+import { HelmetProvider } from "react-helmet-async";
 import "./index.css";
 import App from "./App.tsx";
 import { Analytics } from './Analytics.tsx';
